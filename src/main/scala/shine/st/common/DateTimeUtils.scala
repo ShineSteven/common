7b55f11e9d24c8aeb8e9edad013f8db0ed81112a
ShineSt <fan.steven.chiang@gmail.com>
2016-07-24 16:13:29 +0800

    add date time utils code

--- src/main/scala/shine/st/common/DateTimeUtils.scala
+++ src/main/scala/shine/st/common/DateTimeUtils.scala
@@ -1,6 +1,11 @@
 package shine.st.common
 
-import org.joda.time.format.DateTimeFormat
+import java.util.Locale
+
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+
+import scala.util.Try
 
 /**
   * Created by shinest on 2016/7/9.
@@ -8,4 +13,26 @@ import org.joda.time.format.DateTimeFormat
 object DateTimeUtils {
   val dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
   val dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd")
+  //  Tuesday, June 28, 2016
+  //  english locale
+  val enDateFormat = DateTimeFormat.forPattern("E, MMMM dd, yyyy").withLocale(Locale.ENGLISH)
+
+  def parse(text: String)(formatter: DateTimeFormatter): Option[DateTime] = {
+    Try {
+      formatter.parseDateTime(text)
+    }.toOption
+  }
+
+  def parseDate(text: String) = {
+    parse(text)(dateFormat)
+  }
+
+
+  def parseDateTime(text: String) = {
+    parse(text)(dateTimeFormat)
+  }
+
+  def parseEnDateTime(text: String) = {
+    parse(text)(enDateFormat)
+  }
 }
